c2f4b7bbd6cd1957900730371cef80cda2b8cfc7
frmsm <frmsm@mail.ru>
2018-08-30 22:24:01 +0300

    Resize changed

--- js/data/resize.js
+++ js/data/resize.js
@@ -1,8 +1,7 @@
 export const resize = (frame, given) => {
-  const k1 = (given.width - frame.width) / frame.width + 1;
-  const k2 = (given.height - frame.height) / frame.height + 1;
-  return k1 > k2
-    ? {width: given.width / k1, height: given.height / k1}
-    : {width: given.width / k2, height: given.height / k2};
+  const k1 = given.width / frame.width;
+  const k2 = given.height / frame.height;
+  const k = Math.max(k1, k2);
+  return {width: given.width / k, height: given.height / k};
 };
 
