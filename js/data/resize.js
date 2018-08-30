28a54a7c9b7dd359305f19963fe80a5dfb7a4443
frmsm <frmsm@mail.ru>
2018-08-30 16:10:26 +0300

    changed params in resize function

--- js/data/resize.js
+++ js/data/resize.js
@@ -1,8 +1,8 @@
-export const resize = (expected, natural) => {
-  const k1 = (natural.width - expected.width) / expected.width + 1;
-  const k2 = (natural.height - expected.height) / expected.height + 1;
+export const resize = (frame, given) => {
+  const k1 = (given.width - frame.width) / frame.width + 1;
+  const k2 = (given.height - frame.height) / frame.height + 1;
   return k1 > k2
-    ? {width: natural.width / k1, height: natural.height / k1}
-    : {width: natural.width / k2, height: natural.height / k2};
+    ? {width: given.width / k1, height: given.height / k1}
+    : {width: given.width / k2, height: given.height / k2};
 };
 
